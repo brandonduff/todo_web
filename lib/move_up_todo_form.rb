90a0cd1cb6e02cba3fafcc5546bbddd1a5e22062
brandonduff <bduff@mavenlink.com>
2019-09-27 15:58:49 -0600

    Extract rendering object
    - Maybe can build on this to build a Seaside-like DSL

--- lib/move_up_todo_form.rb
+++ lib/move_up_todo_form.rb
@@ -1,4 +1,22 @@
-class MoveUpTodoForm
+class HTMLRenderer
+  def form(action:, &block)
+    <<~FORM
+      <form action="#{action}" method="post">
+        #{instance_eval(&block)}
+      </form>
+    FORM
+  end
+
+  def submit_button(value:, title:, &block)
+    <<~BTN
+      <button type="submit" value="#{value}" title="#{title}">
+        #{instance_eval(&block)}
+      </button>
+    BTN
+  end
+end
+
+class MoveUpTodoForm < HTMLRenderer
   def self.render(index)
     new(index).render
   end
@@ -8,7 +26,9 @@ class MoveUpTodoForm
   end
 
   def render
-    %(<form action="move_up" method="post"><button type="submit" value="#{index}" title="Move up #{index}>">^</button></form>)
+    form(action: "move_up") do
+      submit_button(value: index, title: "Move up #{index}") { "^" }
+    end
   end
 
   private
