10369bfa0624c0ff3332b4c73fa3754e6ac1c421
Brandon Duff <brandonduff17@gmail.com>
2020-11-01 19:16:54 -0700

    Replace usage of InMemoryPersistence with null Persistence
    - Remove redundant tests

--- test/todo/use_cases/set_current_day_test.rb
+++ test/todo/use_cases/set_current_day_test.rb
@@ -2,34 +2,25 @@ module Todo
   module UseCases
     class SetCurrentDayTest < Minitest::Test
       def setup
-        @persistence = InMemoryPersistence.new
         @today = '10-03-1993'
-        @persistence.write_current_day(@today)
+        @log = Hash.new
+        @null_persistence = Persistence.create_null(current_day: @today, log: @log)
       end
 
       def test_called_with_nil_day_returns_current_day
-        assert_equal(@today, SetCurrentDay.new(new_day: nil, persistence: @persistence).perform)
+        assert_equal(@today, SetCurrentDay.new(new_day: nil, persistence: @null_persistence).perform)
       end
 
-      def test_called_with_new_day_returns_new_day
-        assert_equal(formatted_date('1-1-2000'), SetCurrentDay.new(new_day: '1-1-2000', persistence: @persistence).perform)
+      def test_called_with_new_day_returns_current_day
+        result = SetCurrentDay.new(new_day: '1-1-2000', persistence: @null_persistence).perform
+        assert_equal(@today, result)
+        assert_equal('01-01-2000', @log[@today])
       end
 
-      def test_called_with_new_day_writes_new_day
-        new_day = '1-2-2002'
-        SetCurrentDay.new(new_day: new_day, persistence: @persistence).perform
-        assert_equal(formatted_date(new_day), @persistence.read_current_day)
-      end
-      
       def test_setting_new_day_formats_it
         new_day = 'today'
-        assert_equal(DayFormatter.new(new_day).today, SetCurrentDay.new(new_day: new_day, persistence: @persistence).perform)
-      end
-
-      private
-
-      def formatted_date(date)
-        DayFormatter.new(date).format
+        SetCurrentDay.new(new_day: new_day, persistence: @null_persistence).perform
+        assert_equal(DayFormatter.new(new_day).today, @log[@today])
       end
     end
   end
