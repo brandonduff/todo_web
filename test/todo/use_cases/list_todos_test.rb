79035346c22122f7a306cfff2ac81d64feedf846
Brandon Duff <brandonduff17@gmail.com>
2020-11-01 19:39:01 -0700

    Replace usage of InMemoryPersistence with null Persistence

--- test/todo/use_cases/list_todos_test.rb
+++ test/todo/use_cases/list_todos_test.rb
@@ -1,24 +1,22 @@
 module Todo
   module UseCases
     class ListTodosTest < Minitest::Test
-
       def setup
         @today = '10-03-1993'
-        @persistence = InMemoryPersistence.new
-        @persistence.write_current_day(@today)
+        @log = Hash.new
       end
 
       def test_list_with_no_current_todos_returns_empty_list
-        todos = ListTodos.new(persistence: @persistence).perform
-
+        persistence = Persistence.create_null(current_day: @today)
+        todos = ListTodos.new(persistence: persistence).perform
         assert_equal([], todos)
       end
 
       def test_list_returns_todos_as_array
         task_list = TaskListBuilder.new([{ description: 'hello' }, { description: 'goodbye' }]).build
-        @persistence.write_todays_tasks(task_list)
+        persistence = Persistence.create_null(current_day: @today, tasks: { @today => task_list })
 
-        todos = ListTodos.new(persistence: @persistence).perform
+        todos = ListTodos.new(persistence: persistence).perform
 
         assert_equal(%w(hello goodbye), todos)
       end
@@ -27,23 +25,20 @@ module Todo
         task_list = TaskListBuilder.new([{ description: 'hello' }]).build
         done_todo = Task.new('done', true)
         task_list.add_task(done_todo)
-        @persistence.write_todays_tasks(task_list)
+        persistence = Persistence.create_null(current_day: @today, tasks: { @today => task_list })
 
-        todos = ListTodos.new(all: true, persistence: @persistence).perform
+        todos = ListTodos.new(all: true, persistence: persistence).perform
 
         assert_equal([task_list.to_a[0].formatted_description, done_todo.formatted_description], todos)
       end
 
       def test_week_option
         second_task_list = TaskListBuilder.new([description: 'world']).build
-        @persistence.write_todays_tasks(second_task_list)
         yesterday = '09-03-1993'
-        @persistence.write_current_day(yesterday)
         first_task_list = TaskListBuilder.new([description: 'hello']).build
-        @persistence.write_todays_tasks(first_task_list)
-        @persistence.write_current_day(@today)
+        persistence = Persistence.create_null(current_day: @today, tasks: { yesterday => first_task_list, @today => second_task_list })
 
-        todos = ListTodos.new(week: true, persistence: @persistence).perform
+        todos = ListTodos.new(week: true, persistence: persistence).perform
 
         assert_equal([first_task_list.to_a.first.formatted_description, second_task_list.to_a.first.formatted_description], todos)
       end
@@ -51,24 +46,21 @@ module Todo
       def test_month_option
         last_week = '01-03-1993'
         second_task_list = TaskListBuilder.new([description: 'world']).build
-        @persistence.write_todays_tasks(second_task_list)
-        @persistence.write_current_day(last_week)
         first_task_list = TaskListBuilder.new([description: 'hello']).build
-        @persistence.write_todays_tasks(first_task_list)
-        @persistence.write_current_day(@today)
+        persistence = Persistence.create_null(current_day: @today, tasks: { last_week => first_task_list, @today => second_task_list })
 
-        todos = ListTodos.new(month: true, persistence: @persistence).perform
+        todos = ListTodos.new(month: true, persistence: persistence).perform
 
         assert_equal([first_task_list.to_a.first.formatted_description, second_task_list.to_a.first.formatted_description], todos)
       end
 
       def test_accepts_presenter
         task_list = TaskListBuilder.new([{ description: 'hello', done: false }, { description: 'goodbye', done: false }]).build
-        @persistence.write_todays_tasks(task_list)
+        persistence = Persistence.create_null(current_day: @today, tasks: { @today => task_list })
         console_presenter = double
         allow(console_presenter).to receive(:present).with(task_list.to_a).and_return('presented tasks')
 
-        todos = ListTodos.new(presenter: console_presenter, persistence: @persistence).perform
+        todos = ListTodos.new(presenter: console_presenter, persistence: persistence).perform
 
         assert_equal('presented tasks', todos)
       end
