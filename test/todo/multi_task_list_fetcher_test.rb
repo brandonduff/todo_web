f41f95d6feb74aa3f2b7c3641983635ccbcdcbd2
Brandon Duff <brandonduff17@gmail.com>
2020-11-13 16:01:00 -0700

    Get rid of mocking by using nullable infrastructure

--- test/todo/multi_task_list_fetcher_test.rb
+++ test/todo/multi_task_list_fetcher_test.rb
@@ -1,34 +1,18 @@
 require 'test_helper'
 
 class MultiTaskListFetcherTest < Minitest::Test
-
-  def setup
-    @reader = Object.new
-  end
-
   def test_task_data_for_one_day_range
-    first_day = '10-03-1993'
-    second_day = first_day
-    fetcher = instance_double("TaskListFetcher")
+    day = '10-03-1993'
+    reader = Todo::Notepad.create_null(current_day: '10-03-1993', tasks: { day => 'data' })
 
-    allow(Todo::TaskListFetcher).to receive(:new).with(@reader).and_return(fetcher)
-    allow(fetcher).to receive(:tasks_for_day).with(second_day).and_return(['data'])
-
-    assert_equal(Todo::TaskList.from_array(['data']), Todo::MultiTaskListFetcher.new(@reader, 0).tasks_for_day(second_day))
+    assert_equal(Todo::TaskList.from_array(['data']).to_s, Todo::MultiTaskListFetcher.new(reader, 0).tasks_for_day(day).to_s)
   end
 
   def test_task_data_for_range
-    first_day = '10-03-1993'
-    second_day = '11-03-1993'
-    third_day = '12-03-1993'
-    fetcher = instance_double("TaskListFetcher")
-
-    allow(Todo::TaskListFetcher).to receive(:new).with(@reader).and_return(fetcher)
-    allow(fetcher).to receive(:tasks_for_day).with(first_day).and_return(['first'])
-    allow(fetcher).to receive(:tasks_for_day).with(second_day).and_return(['second'])
-    allow(fetcher).to receive(:tasks_for_day).with(third_day).and_return(['third'])
-
-
-    assert_equal(Todo::TaskList.from_array(%w(first second third)), Todo::MultiTaskListFetcher.new(@reader, 2).tasks_for_day(third_day))
+    first_day = '10-10-1993'
+    second_day = '11-10-1993'
+    third_day = '12-10-1993'
+    reader = Todo::Notepad.create_null(current_day: '10-10-1993', tasks: { first_day => 'first', second_day => 'second', third_day => 'third' })
+    assert_equal(Todo::TaskList.from_array(%w(first second third)).to_s, Todo::MultiTaskListFetcher.new(reader, 2).tasks_for_day(third_day).to_s)
   end
 end
--- test/todo/task_list_fetcher_test.rb
+++ test/todo/task_list_fetcher_test.rb
@@ -3,28 +3,29 @@ require 'test_helper'
 
 class TaskListFetcherTest < Minitest::Test
   def setup
-    @current_day = Date.parse('10-03-1993')
-    @multi_task_fetcher_double = Object.new
-    @notepad = double
-    allow(@notepad).to receive(:read_tasks_for_day).with(@current_day).and_return(Todo::TaskList.from_array([Todo::Task.new('hello', false)]))
+    @current_day = '03-10-1993'
+    @notepad = Todo::Notepad.create_null(current_day: @current_day, tasks: { @current_day => Todo::TaskList.from_array([Todo::Task.new('hello', false)]) })
   end
 
   def test_task_list_for_current_day
-    fetcher = Todo::TaskListFetcher.new(@notepad)
     expected_task_list = Todo::TaskList.new
     expected_task_list.add_task(Todo::Task.new('hello', false))
     assert_equal(fetcher.tasks_for_day(@current_day), expected_task_list)
   end
 
   def test_for_week_returns_multi_task_list_fetcher_for_week
-    allow(Todo::MultiTaskListFetcher).to receive(:new).with(@notepad, 7).and_return(@multi_task_fetcher_double)
-
-    assert_equal(@multi_task_fetcher_double, Todo::TaskListFetcher.new(@notepad).for_week)
+    assert_equal('hello', fetcher.for_week.tasks_for_day('10-10-1993').first.to_s)
+    assert_empty(fetcher.for_week.tasks_for_day('10-11-1993').first.to_s)
   end
 
   def test_for_month_returns_multi_task_list_fetcher_for_month
-    allow(Todo::MultiTaskListFetcher).to receive(:new).with(@notepad, 31).and_return(@multi_task_fetcher_double)
+    assert_equal('hello', fetcher.for_month.tasks_for_day('3-11-1993').first.to_s)
+    assert_empty(fetcher.for_month.tasks_for_day('4-11-1993').first.to_s)
+  end
 
-    assert_equal(@multi_task_fetcher_double, Todo::TaskListFetcher.new(@notepad).for_month)
+  private
+
+  def fetcher
+    Todo::TaskListFetcher.new(@notepad)
   end
 end
