b26a39b70833fd0fe38857c6864c36e2680c85bb
Brandon Duff <brandonduff17@gmail.com>
2020-11-13 16:02:01 -0700

    Replace reader with notepad

--- lib/todo/multi_task_list_fetcher.rb
+++ lib/todo/multi_task_list_fetcher.rb
@@ -1,14 +1,14 @@
 module Todo
   class MultiTaskListFetcher
-    def initialize(reader, days_ago)
-      @reader = reader
+    def initialize(notepad, days_ago)
+      @notepad = notepad
       @days_ago = days_ago
     end
 
     def tasks_for_day(day)
       task_list = TaskList.new
       Range.new(Date.parse(day) - @days_ago, Date.parse(day)).each do |date|
-        task_list = task_list.concat(TaskListFetcher.new(@reader).tasks_for_day(date.strftime("%d-%m-%Y")))
+        task_list = task_list.concat(TaskListFetcher.new(@notepad).tasks_for_day(date.strftime("%d-%m-%Y")))
       end
       task_list
     end
