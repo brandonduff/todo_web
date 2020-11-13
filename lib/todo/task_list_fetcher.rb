78bd5811d146f17078d4e77cebf2541e996e986c
Brandon Duff <brandonduff17@gmail.com>
2020-11-13 16:19:37 -0700

    Collapse Multi task list fetcher
    - Simpler to just configure the task_list_fetcher to do this

--- lib/todo/task_list_fetcher.rb
+++ lib/todo/task_list_fetcher.rb
@@ -2,24 +2,31 @@ module Todo
   class TaskListFetcher
     def initialize(notepad)
       @notepad = notepad
+      @days_ago = 0
     end
 
     def tasks_for_day(day)
-      @notepad.read_tasks_for_day(day)
+      TaskList.new.tap do |task_list|
+        date_range(day).each do |date|
+          task_list.concat(@notepad.read_tasks_for_day(date.strftime("%d-%m-%Y")))
+        end
+      end
     end
 
     def for_week
-      multi_list_fetcher_for_days_ago(7)
+      @days_ago = 7
+      self
     end
 
     def for_month
-      multi_list_fetcher_for_days_ago(31)
+      @days_ago = 31
+      self
     end
 
     private
 
-    def multi_list_fetcher_for_days_ago(days_ago)
-      MultiTaskListFetcher.new(@notepad, days_ago)
+    def date_range(day)
+      (Date.parse(day) - @days_ago)..Date.parse(day)
     end
   end
 end
