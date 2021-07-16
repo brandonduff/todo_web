a90237ba0e893348c5f9811aa2ba2a473ab7b6e3
Brandon Duff <brandonduff17@gmail.com>
2021-07-15 22:55:37 -0600

    Composed method

--- lib/task_list_view.rb
+++ lib/task_list_view.rb
@@ -8,12 +8,21 @@ class TaskListView < HtmlComponent
   end
 
   def render_content_on(html)
+    render_tasks(html)
+    render_new_task_form(html)
+  end
+
+  private
+
+  def render_tasks(html)
     html.unordered_list do
-      @list.each do |todo|
-        html.list_item(todo)
+      @list.each do |task|
+        html.list_item(task)
       end
     end
+  end
 
+  def render_new_task_form(html)
     html.new_form do |f|
       f.text_input(:new_task)
       f.submit_button('create new task')
