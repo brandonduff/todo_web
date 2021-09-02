81ec2da7310a1fc69daa54be8b7a084520487248
Brandon Duff <brandonduff17@gmail.com>
2021-09-02 12:43:24 -0600

    Agenda keeps track of a list per day

--- lib/agenda.rb
+++ lib/agenda.rb
@@ -1,1 +1,18 @@
-Agenda = Struct.new(:current_day, :task_list)
+class Agenda
+  attr_accessor :current_day
+  attr_reader :task_list
+
+  def initialize(current_day, task_list)
+    @current_day = current_day
+    @task_list = task_list
+    @lists = { @current_day => @task_list }
+  end
+
+  def add_list_on_day(list, day)
+    @lists[day] = list
+  end
+
+  def task_list
+    @lists[@current_day]
+  end
+end
--- test/agenda_test.rb
+++ test/agenda_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AgendaTest < Minitest::Test
-  def test_initialization
-    Agenda.new(Date.today, Todo::TaskList.new)
+  def test_holds_separate_lists_for_each_day
+    todays_list = Todo::TaskList.from_array(['do the dishes'])
+    yesterdays_list = Todo::TaskList.from_array(['wash the car'])
+    subject = Agenda.new(Date.today, todays_list)
+    yesterday = Date.today - 1
+
+    subject.add_list_on_day(yesterdays_list, yesterday)
+    subject.current_day = yesterday
+
+    assert_equal yesterdays_list, subject.task_list
   end
 end
