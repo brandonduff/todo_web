e50b6f9a719b987cf7f5f19c2e060dcdf0982b5c
Brandon Duff <brandonduff17@gmail.com>
2021-09-27 18:34:56 -0400

    Extract ContinuationDictionary to own file

--- lib/framework/continuation_dictionary.rb
+++ lib/framework/continuation_dictionary.rb
@@ -18,33 +18,3 @@ class ContinuationDictionary
     @observer = observer
   end
 end
-
-class Continuation
-  def initialize(component, block_or_symbol)
-    @component = component
-    @block_or_symbol = block_or_symbol
-  end
-
-  def call(*args)
-    invoke(args)
-    notify_observers
-  end
-
-  def add_observer(observer)
-    @observer = observer
-  end
-
-  private
-
-  def invoke(args)
-    if @block_or_symbol.is_a?(Proc)
-      @block_or_symbol.call(@component, *args)
-    else
-      @component.send(@block_or_symbol, *args)
-    end
-  end
-
-  def notify_observers
-    @observer.update if @observer
-  end
-end
