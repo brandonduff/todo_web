1e906cce0c87564b2e1bac536461df72029eacaf
Brandon Duff <brandonduff17@gmail.com>
2021-09-28 13:28:19 -0400

    little refactoring

--- lib/framework/html_node.rb
+++ lib/framework/html_node.rb
@@ -5,27 +5,13 @@ class HtmlNode
     @tag_name = tag_name
     @attributes = attributes
     @inner = inner
-    @block = inner if inner.is_a?(Proc)
   end
 
   def to_s(canvas)
     @canvas = canvas
-    append("<#{tag_name}")
-
-    attributes.each do |key, val|
-      attribute_value = val.respond_to?(:call) ? instance_exec(&val) : val
-      append(%( #{key}="#{attribute_value}"))
-    end
-
-    append(">")
-
-    if @block
-      @block.call(@canvas)
-    else
-      append(inner)
-    end
-
-    append("</#{tag_name}>")
+    append_opening_tag
+    append_inner_content
+    append_closing_tag
   end
 
   def append(value)
@@ -34,5 +20,30 @@ class HtmlNode
 
   def buffer
     @canvas.buffer
+  end
+
+  private
+
+  def append_opening_tag
+    append("<#{tag_name}")
+
+    attributes.each do |key, val|
+      attribute_value = val.respond_to?(:call) ? instance_exec(&val) : val
+      append(%( #{key}="#{attribute_value}"))
+    end
+
+    append(">")
+  end
+
+  def append_inner_content
+    if @inner.respond_to?(:call)
+      @inner.call(@canvas)
+    else
+      append(inner)
+    end
+  end
+
+  def append_closing_tag
+    append("</#{tag_name}>")
   end
 end
