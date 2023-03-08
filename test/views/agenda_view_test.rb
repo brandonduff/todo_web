10e268ce6a64ad4b581522b23287a4a1cc74ae6f
brandonduff <bduff@hey.com>
2023-03-07 20:11:41 -0500

    Use method from superclass

--- test/views/agenda_view_test.rb
+++ test/views/agenda_view_test.rb
@@ -1,20 +1,16 @@
 require "test_helper"
 
 class AgendaViewTest < ViewTest
-  def setup
-    @canvas = TestCanvas.build
-  end
-
   def test_current_day_render
     date = Date.today
-    @canvas.render(subject(current_day: date))
-    assert @canvas.rendered?(date)
+    canvas.render(subject(current_day: date))
+    assert canvas.rendered?(date)
   end
 
   def test_todo_list_render
     task = Task.new("do the dishes")
-    @canvas.render(subject(task_list: TaskList.from_array([task])))
-    assert @canvas.rendered?("do the dishes")
+    canvas.render(subject(task_list: TaskList.from_array([task])))
+    assert canvas.rendered?("do the dishes")
   end
 
   def subject(task_list: TaskList.new, current_day: Date.today)
